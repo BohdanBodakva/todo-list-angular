7c6c4ed150ee7670c990c3bbb3c0198d689cff07
Bohdan Bodakva <bodakvabohdan44@gmail.com>
2023-01-22 15:24:32 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, ViewChild } from '@angular/core';
+import { takeLast } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+  title = 'Todo List';
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CalendarModule } from './calendar/calendar.module';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskListModule } from './task-list/task-list.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    TaskListModule,
+    CalendarModule,
+    
+  ],  
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
